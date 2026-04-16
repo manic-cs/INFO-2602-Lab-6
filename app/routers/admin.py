1b9e7b0831d55669708c697ea8744422aecc22ab
manic-cs <rinard.guy@protonmail.com>
2026-04-16 17:32:13 -0400

    Refactor admin_page function for better readability

--- app/routers/admin.py
+++ app/routers/admin.py
@@ -15,29 +15,23 @@ from app.pagination import Pagination
 admin_router = APIRouter(tags=["Admin App"])
 
 @admin_router.get("/admin")
-def admin_page(
-    request: Request, 
-    db: SessionDep, 
-    user: AdminDep, 
-    page: int = Query(default=1, ge=1), 
-    limit: int = Query(default=100, le=100), 
-    q: str = Query(default=''),
-    done: str = Query(default='any')
-):
+def admin_page(request:Request, db:SessionDep, user:AdminDep, page: int = Query(default=1, ge=1), limit: int = Query(default=100, le=100), q: str = Query(default=''), done:str =  Query(default="any")):
     offset = (page - 1) * limit
 
     db_qry = select(Todo).join(User)
-    
     if q:
         db_qry = db_qry.where(
             Todo.text.ilike(f"%{q}%") | User.username.ilike(f"%{q}%")
         )
-    
     if done == "true":
-        db_qry = db_qry.where(Todo.done == True)
-    elif done == "false":
-        db_qry = db_qry.where(Todo.done == False)
-
+        db_qry = db_qry.where(
+            Todo.done == True
+        )
+    elif done == "false": 
+        db_qry = db_qry.where(
+            Todo.done == False
+        )
+        
     count_qry = select(func.count()).select_from(db_qry.subquery())
     count_todos = db.exec(count_qry).one()
 
@@ -51,7 +45,7 @@ def admin_page(
             "current_user": user,
             "todos": todos,
             "pagination": pagination,
-            "q": q,
+            "q":q,
             "done": done
         }
     )
