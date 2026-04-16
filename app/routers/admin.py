bd89ab3f586bd19784b4341205e3391ee45d15e8
manic-cs <rinard.guy@protonmail.com>
2026-04-16 17:21:53 -0400

    Add admin router with admin page endpoint

---
+++ app/routers/admin.py
@@ -0,0 +1,33 @@
+from fastapi import APIRouter, HTTPException, Depends, Request, Response, Form,  Query
+from sqlmodel import select, func
+from math import ceil
+from app.database import SessionDep
+from app.models import *
+from app.auth import *
+from fastapi.security import OAuth2PasswordRequestForm
+from typing import Annotated
+from fastapi import status
+from fastapi.responses import HTMLResponse, RedirectResponse
+from app.utilities import flash
+from . import templates
+from app.pagination import Pagination
+
+admin_router = APIRouter(tags=["Admin App"])
+
+@admin_router.get("/admin")
+def admin_page(request:Request, db:SessionDep, user:AdminDep, page: int = Query(default=1, ge=1), limit: int = Query(default=100, le=100)):
+    offset = (page - 1) * limit
+
+    count_todos = db.exec(select(func.count(Todo.id))).one()
+    todos = db.exec(select(Todo).offset(offset).limit(limit)).all()
+    pagination = Pagination(total_count=count_todos, current_page=page, limit=limit)
+
+    return templates.TemplateResponse(
+        request=request, 
+        name="admin.html",
+        context={
+            "current_user": user,
+            "todos": todos,
+            "pagination": pagination,
+        }
+    )
