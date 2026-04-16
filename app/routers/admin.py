e86f5955df38a444d922e8d1683bfa848400da22
manic-cs <rinard.guy@protonmail.com>
2026-04-16 17:25:48 -0400

    Refactor admin_page function for improved query handling
    Refactored admin_page function to streamline query logic and remove unnecessary conditions.

--- app/routers/admin.py
+++ app/routers/admin.py
@@ -14,29 +14,15 @@ from app.pagination import Pagination
 
 admin_router = APIRouter(tags=["Admin App"])
 
-# Function below
 @admin_router.get("/admin")
-def admin_page(request:Request, db:SessionDep, user:AdminDep, page: int = Query(default=1, ge=1), limit: int = Query(default=100, le=100), q: str = Query(default=''), done:str =  Query(default="any")):
+def admin_page(request:Request, db:SessionDep, user:AdminDep, page: int = Query(default=1, ge=1), limit: int = Query(default=100, le=100), q: str = Query(default='')):
     offset = (page - 1) * limit
 
-    db_qry = select(Todo).join(User)
+    count_todos = db.exec(select(func.count(Todo.id))).one()
     if q:
-        db_qry = db_qry.where(
-            Todo.text.ilike(f"%{q}%") | User.username.ilike(f"%{q}%")
-        )
-    if done == "true": #note the string here
-        db_qry = db_qry.where(
-            Todo.done == True
-        )
-    elif done == "false": 
-        db_qry = db_qry.where(
-            Todo.done == False
-        )
-    # Note that we on'y really care if "done" is true/false, anything else we ignore
-    count_qry = select(func.count()).select_from(db_qry.subquery())
-    count_todos = db.exec(count_qry).one()
-
-    todos = db.exec(db_qry.offset(offset).limit(limit)).all()
+        todos = db.exec(select(Todo).join(User).where(Todo.text.ilike(f"%{q}%") | User.username.ilike(f"%{q}%")).offset(offset).limit(limit)).all()
+    else:
+        todos = db.exec(select(Todo).offset(offset).limit(limit)).all()
     pagination = Pagination(total_count=count_todos, current_page=page, limit=limit)
 
     return templates.TemplateResponse(
@@ -46,7 +32,6 @@ def admin_page(request:Request, db:SessionDep, user:AdminDep, page: int = Query(
             "current_user": user,
             "todos": todos,
             "pagination": pagination,
-            "q":q,
-            "done": done
+            "q":q
         }
     )
